353b9f98a4f117a56256c8a2674d3c4d7c825e35
lucashernandezutec <lucas.hernandez@estudiantes.utec.edu.uy>
2026-08-18 22:09:36 -0300

    Juego entero

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+document.getElementById("jugar")?.addEventListener("click", function () {
+ window.location.href = "prebatalla.html";
+});
+document.getElementById("quees")?.addEventListener("click", function () {
+ window.location.href = "quees.html";
+});
+document.getElementById("trampa")?.addEventListener("click", function () {
+ window.location.href = "trampa.html";
+});
+document.getElementById("queesregresar")?.addEventListener("click", function () {
+ window.location.href = "index.html";
+});
+document.getElementById("tramparegresar")?.addEventListener("click", function () {
+ window.location.href = "index.html";
+});
+document.getElementById("prebatallacontinuar")?.addEventListener("click", function () {
+ window.location.href = "elegir.html";
+});
+document.getElementById("fuerzaboton")?.addEventListener("click", function () {
+ window.location.href = "wfuerza.html";
+});
+document.getElementById("astuciaboton")?.addEventListener("click", function () {
+ window.location.href = "wastucia.html";
+});
+document.getElementById("agilidadboton")?.addEventListener("click", function () {
+ window.location.href = "lagil.html";
+});
+document.getElementById("wfuerzacontinuar")?.addEventListener("click", function () {
+ window.location.href = "wfuerzapost.html";
+});
+document.getElementById("wastuciacontinuar")?.addEventListener("click", function () {
+ window.location.href = "wastuciapost.html";
+});
+document.getElementById("lagilcontinuar")?.addEventListener("click", function () {
+ window.location.href = "lagilpost.html";
+});
+document.getElementById("wfuerzapostcontinuar")?.addEventListener("click", function () {
+ window.location.href = "win.html";
+});
+document.getElementById("wastuciapostcontinuar")?.addEventListener("click", function () {
+ window.location.href = "win.html";
+});
+document.getElementById("lagilpostcontinuar")?.addEventListener("click", function () {
+ window.location.href = "elegir.html";
+});
+document.getElementById("wincontinuar")?.addEventListener("click", function () {
+ window.location.href = "after.html";
+});
+document.getElementById("aftermenu")?.addEventListener("click", function () {
+ window.location.href = "index.html";
+});
